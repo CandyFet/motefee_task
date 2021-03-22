0f1b0577d7dadc539d727469d35d3a51556cd2d8
Fedor Zasechkin <inorect.a.z@gmail.com>
2021-03-22 18:07:20 +0300

    [add] supplier errors locale

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApplicationController < Api::ApplicationController
   def render_error(message, status)
-    render json: { error: true, message: message }, status: status
+    render json: { error: true, message: I18n.t(message, defaul: message) }, status: status
   end
 end
